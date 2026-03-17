538aec38a55e3fc52a1eb6822a484481f2f3234f
ecarreon77 <ecarreon@77global.biz>
2026-03-17 15:02:00 +0800

    Update SecurityConfig CORS

--- src/main/java/com/simple/identity/dto/UserDto.java
+++ src/main/java/com/simple/identity/dto/UserDto.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
 
+    private UUID userId;
     private String email;
     private String firstName;
     private String role;
